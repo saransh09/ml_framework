8edcb8f4887c4386a21f23976afbc4c644b51284
Saransh Agarwal <saransh.agarwal@gmail.com>
2020-05-13 17:29:47 +0530

    [run.sh] predict.py functionality added\n [predict] added code to predict on the basis of different folds (KFOLD)

--- src/train.py
+++ src/train.py
@@ -8,6 +8,7 @@ import joblib
 from . import dispatcher
 
 TRAINING_DATA = os.environ.get("TRAINING_DATA")
+TEST_DATA = os.environ.get("TEST_DATA")
 FOLD = int(os.environ.get("FOLD"))
 MODEL = os.environ.get("MODEL")
 
@@ -21,7 +22,8 @@ FOLD_MAPPPING = {
 
 if __name__ == '__main__':
     
-    df = pd.read_csv(TRAINING_DATA)    
+    df = pd.read_csv(TRAINING_DATA)
+    df_test = pd.read_csv(TEST_DATA)
     train_df = df[df.kfold.isin(FOLD_MAPPPING.get(FOLD))].reset_index(drop=True)
     valid_df = df[df.kfold==FOLD].reset_index(drop=True)
 
@@ -33,13 +35,13 @@ if __name__ == '__main__':
 
     valid_df = valid_df[train_df.columns]
 
-    label_encoder = []
+    label_encoder = {}
     for c in train_df.columns:
         lbl = preprocessing.LabelEncoder()
-        lbl.fit(train_df[c].values.tolist() + valid_df[c].values.tolist())
+        lbl.fit(train_df[c].values.tolist() + valid_df[c].values.tolist() + df_test[c].values.tolist())
         train_df.loc[:,c] = lbl.transform(train_df[c].values.tolist())
         valid_df.loc[:,c] = lbl.transform(valid_df[c].values.tolist())
-        label_encoder.append((c,lbl))
+        label_encoder[c] = lbl
 
     # data is ready to train
     clf = dispatcher.MODELS[MODEL]
@@ -47,5 +49,6 @@ if __name__ == '__main__':
     preds = clf.predict_proba(valid_df)[:,1]
     print(metrics.roc_auc_score(y_valid, preds))
 
-    joblib.dump(label_encoder, f'models/{MODEL}_label_encoder.pkl')
-    joblib.dump(clf, f'models/{MODEL}.pkl')
+    joblib.dump(label_encoder, f'models/{MODEL}_{FOLD}_label_encoder.pkl')
+    joblib.dump(clf, f'models/{MODEL}_{FOLD}.pkl')
+    joblib.dump(train_df.columns, f"models/{MODEL}_{FOLD}_columns.pkl")
